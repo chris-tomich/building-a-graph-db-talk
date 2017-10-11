b45211bf80734358d38ed3e6cb761d634349618e
Chris Tomich <chris.tomich@gmail.com>
2017-10-11 12:52:56 +0800

    Finished channel vs mutex example

--- channel-shared-map/main.go
+++ channel-shared-map/main.go
@@ -1,4 +1,90 @@
 package main
 
+import "fmt"
+
 func main() {
+	sharedMap := NewChannelSharedMap()
+
+	go sharedMap.WriteToMap("Emma", "A message from Liam to Emma")
+	go sharedMap.WriteToMap("Liam", "A message from Emma to Liam")
+	go sharedMap.WriteToMap("Olivia", "A message from Noah to Olivia")
+	go sharedMap.WriteToMap("Noah", "A message from Olivia to Noah")
+	go sharedMap.WriteToMap("Ava", "A message from Isabella to Ava")
+	go sharedMap.WriteToMap("Lucas", "A message from Mason to Lucas")
+	go sharedMap.WriteToMap("Isabella", "A message from Ava to Isabella")
+	go sharedMap.WriteToMap("Mason", "A message from Lucas to Mason")
+	go sharedMap.WriteToMap("Sophia", "A message from Logan to Sophia")
+	go sharedMap.WriteToMap("Logan", "A message from Sophia to Logan")
+
+	go sharedMap.ReadFromMap("Emma")
+	go sharedMap.ReadFromMap("Liam")
+	go sharedMap.ReadFromMap("Olivia")
+	go sharedMap.ReadFromMap("Noah")
+	go sharedMap.ReadFromMap("Ava")
+	go sharedMap.ReadFromMap("Lucas")
+	go sharedMap.ReadFromMap("Isabella")
+	go sharedMap.ReadFromMap("Mason")
+	go sharedMap.ReadFromMap("Sophia")
+	go sharedMap.ReadFromMap("Logan")
+
+	<-sharedMap.Done
+}
+
+func NewChannelSharedMap() *ChannelSharedMap {
+	m := &ChannelSharedMap{
+		sharedMap: make(map[string]string),
+		write:     make(chan *Message),
+		read:      make(chan string),
+		Done:      make(chan bool, 1),
+	}
+
+	go m.Loop()
+
+	return m
+}
+
+type Message struct {
+	Recipient string
+	Message   string
+}
+
+type ChannelSharedMap struct {
+	sharedMap map[string]string
+	write     chan *Message
+	read      chan string
+	Done      chan bool
+}
+
+func (m *ChannelSharedMap) Loop() {
+	for {
+		select {
+		case msg := <-m.write:
+			if msg != nil {
+				m.sharedMap[msg.Recipient] = msg.Message
+			}
+		case recipient := <-m.read:
+			message := m.sharedMap[recipient]
+
+			if message == "" {
+				go m.ReadFromMap(recipient)
+				continue
+			}
+
+			delete(m.sharedMap, recipient)
+			fmt.Println(message)
+
+			if len(m.sharedMap) == 0 {
+				m.Done <- true
+				return
+			}
+		}
+	}
+}
+
+func (m *ChannelSharedMap) WriteToMap(recipient string, message string) {
+	m.write <- &Message{Recipient: recipient, Message: message}
+}
+
+func (m *ChannelSharedMap) ReadFromMap(recipient string) {
+	m.read <- recipient
 }
--- mutex-shared-map/main.go
+++ mutex-shared-map/main.go
@@ -1,4 +1,72 @@
 package main
 
+import (
+	"sync"
+	"fmt"
+)
+
 func main() {
+	sharedMap := NewMutexSharedMap()
+
+	wg1 := &sync.WaitGroup{}
+
+	go sharedMap.WriteToMap(wg1, "Emma", "A message from Liam to Emma")
+	go sharedMap.WriteToMap(wg1, "Liam", "A message from Emma to Liam")
+	go sharedMap.WriteToMap(wg1, "Olivia", "A message from Noah to Olivia")
+	go sharedMap.WriteToMap(wg1, "Noah", "A message from Olivia to Noah")
+	go sharedMap.WriteToMap(wg1, "Ava", "A message from Isabella to Ava")
+	go sharedMap.WriteToMap(wg1, "Lucas", "A message from Mason to Lucas")
+	go sharedMap.WriteToMap(wg1, "Isabella", "A message from Ava to Isabella")
+	go sharedMap.WriteToMap(wg1, "Mason", "A message from Lucas to Mason")
+	go sharedMap.WriteToMap(wg1, "Sophia", "A message from Logan to Sophia")
+	go sharedMap.WriteToMap(wg1, "Logan", "A message from Sophia to Logan")
+
+	wg1.Add(10)
+	wg1.Wait()
+
+	wg2 := &sync.WaitGroup{}
+
+	go sharedMap.ReadFromMap(wg2, "Emma")
+	go sharedMap.ReadFromMap(wg2, "Liam")
+	go sharedMap.ReadFromMap(wg2, "Olivia")
+	go sharedMap.ReadFromMap(wg2, "Noah")
+	go sharedMap.ReadFromMap(wg2, "Ava")
+	go sharedMap.ReadFromMap(wg2, "Lucas")
+	go sharedMap.ReadFromMap(wg2, "Isabella")
+	go sharedMap.ReadFromMap(wg2, "Mason")
+	go sharedMap.ReadFromMap(wg2, "Sophia")
+	go sharedMap.ReadFromMap(wg2, "Logan")
+
+	wg2.Add(10)
+	wg2.Wait()
+}
+
+func NewMutexSharedMap() *MutexSharedMap {
+	m := &MutexSharedMap{
+		sharedMap: make(map[string]string),
+		RWMutex: &sync.RWMutex{},
+	}
+
+	return m
+}
+
+type MutexSharedMap struct {
+	sharedMap map[string]string
+	*sync.RWMutex
+}
+
+func (m *MutexSharedMap) WriteToMap(wg *sync.WaitGroup, recipient string, message string) {
+	m.Lock()
+	m.sharedMap[recipient] = message
+	m.Unlock()
+	wg.Done()
+}
+
+func (m *MutexSharedMap) ReadFromMap(wg *sync.WaitGroup, recipient string) {
+	m.RLock()
+	message := m.sharedMap[recipient]
+	m.RUnlock()
+
+	fmt.Println(message)
+	wg.Done()
 }
